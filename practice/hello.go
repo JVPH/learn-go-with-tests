c5a54a17a08adfb29c0b8df7b554c0dc6d0a420f
JVPH <gyrospizzeria@protonmail.com>
2024-05-22 21:03:13 -0500

    refactor: add spanish and french

--- practice/hello.go
+++ practice/hello.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return fmt.Sprintf("%s%s", spanishHelloPrefix, name)
+	}
+	if language == "French" {
+		return fmt.Sprintf("%s%s", frenchHelloPrefix, name)
 	}
 	return fmt.Sprintf("%s%s", englishHelloPrefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
