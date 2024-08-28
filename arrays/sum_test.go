bac6dfd7112c5446b5efaf6ac9278e4f9051105e
JVPH <gyrospizzeria@protonmail.com>
2024-08-28 17:16:41 -0500

    Arrays and slices: refactor to use "range"

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,16 +1,27 @@
 package arrays
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
 		got := Sum(numbers)
-		expected := 6
+		want := 6
 
-		if got != expected {
-			t.Errorf("got %d expected %d given %v", got, expected, numbers)
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
 		}
 	})
+}
 
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
